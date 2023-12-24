6928ec3719bd5bd311530c092a04b1ec311358c5
MrRuslanYT <pandacorpglobal@gmail.com>
2023-12-24 15:31:56 +0200

    Enabled ViewBinding
    (cherry picked from commit a6ffaf6ec0889e165a28c0ece6f42f58244c687f)

--- app/src/main/java/com/pandacorp/togetheraichat/MainActivity.kt
+++ app/src/main/java/com/pandacorp/togetheraichat/MainActivity.kt
@@ -2,10 +2,20 @@ package com.pandacorp.togetheraichat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pandacorp.togetheraichat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
