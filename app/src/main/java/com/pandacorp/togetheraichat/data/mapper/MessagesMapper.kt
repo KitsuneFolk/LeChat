b8a222cf0bb0c53ed0d5c12595125163672a2d0a
MrRuslanYT <pandacorpglobal@gmail.com>
2024-01-03 14:49:22 +0200

    Added MessagesMapper.kt
    (cherry picked from commit 5b353988afdde1c294538967ffd04d812370c1bb)

---
+++ app/src/main/java/com/pandacorp/togetheraichat/data/mapper/MessagesMapper.kt
@@ -0,0 +1,21 @@
+package com.pandacorp.togetheraichat.data.mapper
+
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.pandacorp.togetheraichat.domain.model.MessageItem
+
+class MessagesMapper {
+    fun toChatMessages(list: List<MessageItem>): List<ChatMessage> {
+        return list.map {
+            val role = ChatRole(
+                when (it.role) {
+                    MessageItem.SYSTEM -> ChatRole.System.role
+                    MessageItem.USER -> ChatRole.User.role
+                    MessageItem.AI -> ChatRole.Assistant.role
+                    else -> throw IllegalArgumentException("Unknown role: ${it.role}")
+                }
+            )
+            ChatMessage(role = role, content = it.message)
+        }
+    }
+}
--- app/src/main/java/com/pandacorp/togetheraichat/di/modules/koinModule.kt
+++ app/src/main/java/com/pandacorp/togetheraichat/di/modules/koinModule.kt
@@ -1,12 +1,15 @@
 package com.pandacorp.togetheraichat.di.modules
 
+import com.pandacorp.togetheraichat.data.mapper.MessagesMapper
 import com.pandacorp.togetheraichat.data.repository.TogetherRepositoryImpl
 import com.pandacorp.togetheraichat.domain.repository.TogetherRepository
 import com.pandacorp.togetheraichat.presentation.vm.MessagesViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val koinModule = module {
+    singleOf(::MessagesMapper)
     single<TogetherRepository> {
         TogetherRepositoryImpl()
     }
