015b5110b154e2fde4ed395487f3969117742e93
MrRuslanYT <pandacorpglobal@gmail.com>
2024-02-07 12:33:48 +0200

    Removed tests in :openai-kotlin

--- openai-kotlin/build.gradle.kts
+++ openai-kotlin/build.gradle.kts
@@ -10,8 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -31,13 +29,9 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("com.squareup.okio:okio:3.4.0")
     implementation("io.ktor:ktor-client-auth:2.3.2")
