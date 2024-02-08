9fc5ed8688aadeef8b4ba16c2a6a2c6050cbee4b
MrRuslanYT <pandacorpglobal@gmail.com>
2024-02-08 14:54:12 +0200

    Removed empty proguard and consumer rules

--- openai-kotlin/build.gradle.kts
+++ openai-kotlin/build.gradle.kts
@@ -10,13 +10,12 @@ android {
 
     defaultConfig {
         minSdk = 21
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
     compileOptions {
