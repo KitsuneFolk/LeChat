92cd9daee0ff5e71cd5d29e46669a979025fabd2
MrRuslanYT <pandacorpglobal@gmail.com>
2024-02-25 14:34:25 +0200

    Replaced a deprecated field call

--- DropSpinner/src/main/java/com/pandacorp/dropspinner/RecyclerViewAdapter.kt
+++ DropSpinner/src/main/java/com/pandacorp/dropspinner/RecyclerViewAdapter.kt
@@ -39,7 +39,7 @@ internal class RecyclerViewAdapter : ListAdapter<DropDownItem, RecyclerViewAdapt
             }
 
             binding.root.setOnClickListener {
-                onItemClickListener(adapterPosition)
+                onItemClickListener(bindingAdapterPosition)
             }
 
             bindText(item.text)
